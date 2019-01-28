2f7643040373af4f4ce317c7ab092012af8248b1
Alex <alxford45@gmail.com>
2019-01-28 15:05:54 -0600

    added citations

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
+// Typescript configuration file copied from next.config.js example in zeit/next-plugins repo
+//
+// Source: https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
+// Version: commit 8d58b41 on Dec 15, 2018
+// Accessed: January 2019
+
 const typescript = require("@zeit/next-typescript");
 module.exports = typescript();
