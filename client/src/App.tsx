a0a6f53a0f1cedd76d101b6c630c39e36e906307
Daniel <dgiang1@lsu.edu>
2019-02-10 20:23:01 -0600

    added classes for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
+import Dashboard from "./pages/Dashboard";
+import Edit from "./pages/Edit";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import View from "./pages/View";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
@@ -9,6 +15,14 @@ class App extends Component {
       <div>
         <NavBar />
         <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Dashboard" component={Dashboard} />
+            <Route path="/View" component={View} />
+            <Route path="/SignUp" component={SignUp} />
+            <Route path="/Edit" component={Edit} />
+            <Route path="/Login" component={Login} />
+          </Switch>
         </Router>
       </div>
     )
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                SignUp works
+            </div>
+        )
+    }
+}
+export default SignUp;
