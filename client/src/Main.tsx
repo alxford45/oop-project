dc07efb9c79c1eda924fe34e0b9f96686cc094c6
Alex <alxford45@gmail.com>
2019-02-10 19:49:43 -0600

    fixed formatting

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -3,4 +3,9 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
