d657db646a1b136ae75beb75d31c625d31e9e98f
Daniel <dgiang1@lsu.edu>
2019-02-10 21:06:30 -0600

    added drawer to NavBar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,21 +4,39 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import React from "react";
+import React, { Component } from "react";
+import { Drawer } from "@material-ui/core";
 
-const NavBar = () => (
-  <div className="AppBar">
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton color="inherit" aria-label="Menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          Home
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  </div>
-);
+export interface State {
+  isOpen: boolean;
+}
+
+class NavBar extends Component<State> {
+  toggleDrawer = () => {
+    this.toggleDrawer.bind(this);
+    this.state.isOpen = !this.state.isOpen;
+  };
+  state: State = {
+    isOpen: false
+  };
+  render() {
+    return (
+      <div className="AppBar">
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu" onClick={this.toggleDrawer}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer open={this.state.isOpen} />
+            <Typography variant="h6" color="inherit">
+              Home
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+
 export default NavBar;
