acb0d7c5977e4ff878c420643d4172569d37642d
Daniel <dgiang1@lsu.edu>
2019-02-15 20:08:25 -0600

    Added list to the edit page

--- client/src/pages/Edit.tsx
+++ client/src/pages/Edit.tsx
@@ -1,12 +1,67 @@
-import React, { Component } from "react";
+import React from 'react';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
 
-class Edit extends Component {
+class NestedList extends React.Component {
+    state = {
+        open: true,
+    };
+
+    handleClick = () => {
+        this.setState(state => ({ open: !this.state.open }));
+    };
+
     render() {
+
         return (
-            <div>
-                Edit works
-            </div>
-        )
+            <List
+                component="nav"
+                subheader={<ListSubheader component="div">Favorite Artists</ListSubheader>}
+            >
+                <ListItem button >
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText inset primary="Artist #1" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <DraftsIcon />
+                    </ListItemIcon>
+                    <ListItemText inset primary="Artist #2" />
+                </ListItem>
+                <ListItem button onClick={this.handleClick}>
+                    <ListItemIcon>
+                        <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText inset primary="Artist #3" />
+                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <List >
+                        <ListItem button >
+                            <ListItemIcon>
+                                <StarBorder />
+                            </ListItemIcon>
+                            <ListItemText inset primary="Album #1" />
+                        </ListItem>
+                    </List>
+                </Collapse>
+            </List>
+        );
     }
 }
-export default Edit;
+
+
+
+export default (NestedList);
