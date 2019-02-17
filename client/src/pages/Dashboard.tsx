448657f6cc680d3c0a5cc851414517b838836f23
Daniel <dgiang1@lsu.edu>
2019-02-17 01:22:21 -0600

    Edited grid layout on dashboard page

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -6,6 +6,9 @@ import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -15,13 +18,12 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from "@material-ui/core/Button";
-import Edit from "./Edit";
-import Router from "react-router-dom";
+import ImageIcon from '@material-ui/icons/Image';
+
 
 const drawerWidth = 240;
 
@@ -103,27 +105,47 @@ const styles = (theme: Theme) => ({
     },
 });
 
+function ArtistList() {
+    return (
+        <List>
+            <ListItem button>
+                <ListItemIcon>
+                    <ImageIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="Artist #1" />
+            </ListItem>
+            <ListItem button>
+                <ListItemIcon>
+                    <ImageIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="Artist #2" />
+            </ListItem>
+            <ListItem button>
+                <ListItemIcon>
+                    <ImageIcon />
+                </ListItemIcon>
+                <ListItemText inset primary="Artist #3" />
+            </ListItem>
+        </List>
+    );
+}
+
 function MediaCard() {
     return (
         <Card >
-            <CardActionArea>
-                <CardMedia
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title="Contemplative Reptile"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        List Title
+            <CardMedia
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="Contemplative Reptile"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    List Title
                     </Typography>
-                    <Typography gutterBottom variant="h6" component="h2">
-                        2/16/19
+                <Typography gutterBottom variant="h6" component="h2">
+                    2/16/19
                     </Typography>
-                    <Typography component="p">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                {ArtistList()}
+            </CardContent>
             <CardActions>
                 <Button size="small" color="primary" >
                     Edit
@@ -138,7 +160,22 @@ function MediaCard() {
 
 function Grids() {
     return (
-        <Grid container spacing={40}>
+        <Grid container spacing={40} >
+            <Grid item sm={6} md={4} lg={4}>
+                <Card >
+                    {MediaCard()}
+                </Card>
+            </Grid>
+            <Grid item sm={6} md={4} lg={4}>
+                <Card >
+                    {MediaCard()}
+                </Card>
+            </Grid>
+            <Grid item sm={6} md={4} lg={4}>
+                <Card >
+                    {MediaCard()}
+                </Card>
+            </Grid>
             <Grid item sm={6} md={4} lg={4}>
                 <Card >
                     {MediaCard()}
@@ -157,8 +194,6 @@ function Grids() {
         </Grid>
     );
 }
-
-
 
 class Dashboard extends React.Component {
     state = {
