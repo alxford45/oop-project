ce9857dd72a34685aeab45880a0e90b53fa83980
Alex <alxford45@gmail.com>
2019-03-17 17:03:48 -0500

    testing express and apollo integration

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const ArtistAPI = require("./data/artist");
+
+const PORT = 8000;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -11,6 +15,7 @@ const server = new ApolloServer({
     };
   }
 });
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+server.applyMiddleware({ app, path: "/" });
+app.listen({ port: PORT }, () => {
+  console.log(`Server ready at http://localhost:${PORT}`);
 });
