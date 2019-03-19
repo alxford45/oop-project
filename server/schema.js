e7d1fe0fa655c7b0622f0783bf27b83fe7c6dcc3
Alex <alxford45@gmail.com>
2019-03-19 15:57:37 -0500

    implemented Artist type

--- server/schema.js
+++ server/schema.js
@@ -5,16 +5,17 @@ const typeDefs = gql`
   type Query {
     artist(id: String!): Artist
   }
+
   type Artist {
-    name: String!
-  }
-  type _Artist {
     genres: [String!]
     id: String!
     name: String!
     popularity: Int
     type: String!
     uri: String!
+  }
+  type Artist_topTracks {
+    artists: [Artist]
   }
 `;
 
