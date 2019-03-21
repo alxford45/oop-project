af6f1af3c454ea3949dd63fab0bd76d00d4a2e22
Alex <alxford45@gmail.com>
2019-03-21 12:10:45 -0500

    added non-nullable props to Artist and Query types

--- server/schema/typeDefs.ts
+++ server/schema/typeDefs.ts
@@ -2,13 +2,13 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Query {
-    artist(id: String): Artist
+    artist(id: String!): Artist
   }
   type Artist {
     genres: [String!]
     id: String!
     name: String!
-    popularity: Int
+    popularity: Int!
     type: String!
     uri: String!
   }
