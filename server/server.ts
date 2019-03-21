3ba6e054cebd793c9d32a5519a6e71e2d53f7e2a
Alex <alxford45@gmail.com>
2019-03-21 12:07:45 -0500

    moved typeDefs and resolvers to schema directory

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from "apollo-server-express";
 
 import ArtistAPI from "./data/ArtistAPI";
-import { typeDefs } from "./typeDefs";
-import { resolvers } from "./resolvers";
+import { typeDefs } from "./schema/typeDefs";
+import { resolvers } from "./schema/resolvers";
 
 export const server = new ApolloServer({
   typeDefs,
