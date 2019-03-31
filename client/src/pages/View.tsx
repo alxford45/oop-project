932410d7cdbe3583788f2fa8f30dd86487f9c0a1
Daniel <dgiang1@lsu.edu>
2019-03-31 15:53:33 -0500

    added DashMenu to View page

--- client/src/pages/View.tsx
+++ client/src/pages/View.tsx
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
 import MediaCard from "../components/MediaCard";
-import { Card, List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { Card, List, ListItem, ListItemIcon, ListItemText, Theme, createStyles, WithStyles, withStyles } from "@material-ui/core";
+import DashBar from "../components/DashBar";
 
 
+const styles = (theme: Theme) => createStyles({
+    root: {
+        display: "flex"
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        height: "100vh",
+        overflow: "auto"
+    },
+});
 
-class View extends Component<{ location: object }> {
+
+class View extends Component<WithStyles<typeof styles>> {
 
 
 
     render() {
-
+        const { classes } = this.props;
         return (
-            <div>
-                View Works
+            <div className={this.props.classes.root}>
+                <DashBar />
+                <main className={classes.content}>
+                    <div className={classes.appBarSpacer} />
+
+                </main>
             </div>
         )
     }
 }
-export default View;
+export default withStyles(styles)(View);
