f8f9fa4350da8fb926c53c8f93d4d48e87152e75
Alex <alxford45@gmail.com>
2019-03-31 15:59:42 -0500

    added working auth promise implementation to context inorder to authenticate each request to spotify endpoints

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,50 @@
 import { ApolloServer } from "apollo-server-express";
-
 import ArtistAPI from "./api/ArtistAPI";
 import { typeDefs } from "./schema/typeDefs";
 import { resolvers } from "./schema/resolvers";
 
+import * as _ from "./env";
+import * as rp from "request-promise";
+
 export const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req, res }: any) => {
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+
+    const authOptions = {
+      method: "POST",
+      uri: "https://accounts.spotify.com/api/token",
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(`${client_id}:${client_secret}`).toString("base64")
+      },
+      form: {
+        grant_type: "client_credentials"
+      },
+      json: true
+    };
+
+    const getToken = () => {
+      return rp(authOptions)
+        .then(parsedBody => {
+          const tokType = parsedBody.token_type;
+          const tokVal = parsedBody.access_token;
+          return `${tokType} ${tokVal}`;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+    const token = getToken();
+    return { req, res, token };
+  },
+
   dataSources: () => {
     return {
       artistAPI: new ArtistAPI()
     };
-  },
-  context: ({ req, res }: any) => ({ req, res })
+  }
 });
