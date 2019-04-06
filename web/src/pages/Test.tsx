0b889d43146aaa5a05d3517564fd741f0eaf37a2
Alex <alxford45@gmail.com>
2019-04-05 21:01:03 -0500

    added test page to master from bridge

---
+++ web/src/pages/Test.tsx
@@ -0,0 +1,65 @@
+import { gql } from "apollo-boost";
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+
+const searchQuery = gql`
+  query SearchQuery($name: String!) {
+    search(name: $name) {
+      artists {
+        href
+        ...ItemsFragment
+      }
+    }
+  }
+  fragment ItemsFragment on Results {
+    items {
+      id
+      name
+    }
+  }
+`;
+class Test extends Component {
+    state = {
+        search: ""
+    };
+    handleChange = e => {
+        this.state.search = e;
+    };
+    get = ({ name }) => {
+        return (
+            <Query query={searchQuery} variables={{ name }}>
+                {({ loading, error, data }) => {
+                    if (loading) return <div>loading</div>;
+                    console.log(data.search.artists.items);
+                    return (
+                        <ul>
+                            {data.search.artists.items.map(({ name, id }, index) => {
+                                return (
+                                    <li key={index}>
+                                        <div>
+                                            <strong>name</strong>: {name} <br />
+                                            <strong>id</strong>: {id} <br />
+                                            <br />
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    );
+                }}
+            </Query>
+        );
+    };
+    render() {
+        return (
+            <div>
+                <form>
+                    Search: <input type="text" onChange={this.handleChange} />
+                </form>
+                <button>Search</button>
+                <div>{this.get({ name: "pitbull" })}</div>
+            </div>
+        );
+    }
+}
+export default Test;
