50ae9c43ab51b6587f0f03713a75be788d300dfd
Daniel <dgiang1@lsu.edu>
2019-04-22 21:36:31 -0500

    Calendar changes

--- web/src/components/Calendar.tsx
+++ web/src/components/Calendar.tsx
@@ -29,46 +29,10 @@ class Calendar extends React.Component<any, any> {
     var dates = [""];
     var months = [""];
     var eventDates = ["01-02-03"];
-    this.state.event.get().map((item, index) => eventDates.push(item.day));
+    this.state.event.get().map((item) => eventDates.push(item.day));
     eventDates.map(element => {
       let pieces = element.split("-");
       dates.push(pieces[2]);
-      if (pieces[1] == "01") {
-        pieces[1] = "Jan";
-      }
-      if (pieces[1] == "02") {
-        pieces[1] = "Feb";
-      }
-      if (pieces[1] == "03") {
-        pieces[1] = "Mar";
-      }
-      if (pieces[1] == "04") {
-        pieces[1] = "Apr";
-      }
-      if (pieces[1] == "05") {
-        pieces[1] = "May";
-      }
-      if (pieces[1] == "06") {
-        pieces[1] = "Jun";
-      }
-      if (pieces[1] == "07") {
-        pieces[1] = "Jul";
-      }
-      if (pieces[1] == "08") {
-        pieces[1] = "Aug";
-      }
-      if (pieces[1] == "09") {
-        pieces[1] = "Sep";
-      }
-      if (pieces[1] == "10") {
-        pieces[1] = "Oct";
-      }
-      if (pieces[1] == "11") {
-        pieces[1] = "Nov";
-      }
-      if (pieces[1] == "12") {
-        pieces[1] = "Dec";
-      }
       months.push(pieces[1]);
     });
     dates.splice(0, 2);
@@ -90,14 +54,12 @@ class Calendar extends React.Component<any, any> {
         event.add(newEvent)
       )
     );
-
     this.setState({ event: event });
     this.setState({ loaded: true });
   };
 
   renderHeader() {
     const dateFormat = "MMMM YYYY";
-
     return (
       <div className="header row flex-middle">
         <div className="col col-start">
@@ -116,6 +78,7 @@ class Calendar extends React.Component<any, any> {
   }
 
   renderDays() {
+
     const dateFormat = "dddd";
     const days = [<div />];
 
@@ -133,7 +96,7 @@ class Calendar extends React.Component<any, any> {
   }
 
   renderCells() {
-    const { currentMonth, selectedDate } = this.state;
+    const { currentMonth } = this.state;
     const monthStart = dateFns.startOfMonth(currentMonth);
     const monthEnd = dateFns.endOfMonth(monthStart);
     const startDate = dateFns.startOfWeek(monthStart);
@@ -145,18 +108,21 @@ class Calendar extends React.Component<any, any> {
     let days = [<div />];
     let day = startDate;
     let formattedDate = "";
+    const monthFormat = "MM";
+
     while (day <= endDate) {
       for (let i = 0; i < 7; i++) {
         formattedDate = dateFns.format(day, dateFormat);
         const cloneDay = day;
         let stringDate = dateFns.parse(cloneDay).toString();
         let splitDate = stringDate.split(" ");
+        splitDate[1] = dateFns.format(this.state.currentMonth, monthFormat);
         days.push(
           <div className={`col cell`} key={cloneDay.toString()}>
             <span className="number">
               {this.state.event
                 .get()
-                .map((item, index) =>
+                .map((item) =>
                   item.day == splitDate[2] && item.months == splitDate[1] ? (
                     <Button
                       onClick={() => this.onDateClick(splitDate[2], item)}
@@ -183,9 +149,13 @@ class Calendar extends React.Component<any, any> {
   }
 
   onDateClick = (date, item) => {
+
+
     if (date == item.day) {
       this.setState({ details: item });
+
     }
+
     this.setState({ open: true });
   };
 
