1263d2642354dde381e66c48c2ae7f74f1433067
Alex <alxford45@gmail.com>
2019-06-12 17:16:14 -0500

    removed duplicate unused files

--- server/src/schema/typeDefs.ts
+++
@@ -1,7 +0,0 @@
-import { typeDef as query } from "./typeDefs/query";
-import { typeDef as artist } from "./typeDefs/artist";
-import { typeDef as user } from "./typeDefs/user";
-import { typeDef as mutation } from "./typeDefs/mutation";
-import { typeDef as search } from "./typeDefs/search";
-
-export const typeDefs = [query, mutation, artist, user, search];
