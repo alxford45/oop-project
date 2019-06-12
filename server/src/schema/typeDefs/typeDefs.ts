b137706f5a86b3ea49824c0edddf64f75d91b2da
Alex <alxford45@gmail.com>
2019-06-12 17:01:55 -0500

    fixed type mismatch

--- server/src/schema/typeDefs/typeDefs.ts
+++ server/src/schema/typeDefs/typeDefs.ts
@@ -1,7 +1,7 @@
-import { queries as query } from "./query";
+import { typeDef as query } from "./query";
 import { typeDef as artist } from "./spotify/artist";
 import { typeDef as user } from "./spotify/user";
-import { mutations as mutation } from "./mutation";
+import { typeDef as mutation } from "./mutation";
 import { typeDef as search } from "./spotify/search";
 import { typeDef as artistSearch } from "./songkick/search";
 import { typeDef as albums } from "./spotify/albums";
