70785d590ee3dd6bfcc178b78c68b2381b7e8014
Alex <alxford45@gmail.com>
2019-06-12 19:55:49 -0500

    added lists field stub (currently not working)

--- server/src/schema/typeDefs/user.ts
+++ server/src/schema/typeDefs/user.ts
@@ -4,5 +4,8 @@ export const typeDef = gql`
   type User {
     id: ID!
     email: String!
+    lists: [List]
   }
 `;
+//note lists field currently not working and will always display none.
+//future ToDo: join lists column to user in psql
