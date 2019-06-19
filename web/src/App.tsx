2f0228f8020fb1181f6b43bedbef5d834c7b1ef3
Alex <alxford45@gmail.com>
2019-06-19 14:12:21 -0500

    removed callbacks, list state, and routes for refactoring

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,42 +6,13 @@ import {
   Switch
 } from "react-router-dom";
 import { ArtistList } from "./list/ArtistList";
-import { BigList } from "./list/BigList";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Edit from "./pages/Edit/Edit";
 import Home from "./pages/Home";
-import Login from "./pages/Login/Form";
+import Login from "./pages/Login/Login";
 import Collection from "./pages/Collection/Collection";
 
-const list = new ArtistList();
-const bigList = new BigList();
-
-interface State {
-  bigList: BigList;
-  list: ArtistList;
-}
-
-class App extends React.Component<{}, State> {
-  state = {
-    bigList: bigList,
-    list: list
-  };
-
-  //Takes the list data from the Edit page and stores it in App's state.
-  editCallback = data => {
-    bigList.add(data);
-  };
-
-  //Takes the specific list chosen on dashboard and sends its data to the view page.
-  passListToView = data => {
-    this.setState({ list: data });
-  };
-
-  deleteList = data => {
-    bigList.remove(data);
-    this.setState({ bigList: bigList });
-  };
-
+class App extends React.Component {
   render() {
     return (
       <Router>
@@ -52,24 +23,7 @@ class App extends React.Component<{}, State> {
               <Redirect to="/" />
             </Route>
             <Route path="/login" component={Login} />
-            <Route
-              path="/dashboard"
-              render={props => (
-                <Dashboard
-                  {...props}
-                  callbackView={this.passListToView}
-                  callbackDelete={this.deleteList}
-                  bigList={bigList}
-                />
-              )}
-            />
-            <Route
-              path="/edit"
-              render={props => <Edit {...props} callback={this.editCallback} />}
-            />
-            <Route
-              path="/view"
-              render={props => <Collection {...props} list={this.state.list} />}
+            <Route path="/dashboard" component={Dashboard} />
             />
           </Switch>
         </div>
