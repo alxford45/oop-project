993313bf9b2a34803600ea1478cc054c88960d03
Alex <alxford45@gmail.com>
2019-06-26 19:56:04 -0500

    reformatted to function

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,29 +6,27 @@ import {
   Switch
 } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Edit from "./pages/Edit/Edit";
+import { Edit } from "./pages/Edit/Edit";
 import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/Home">
-              <Redirect to="/" />
-            </Route>
-            <Route path="/Login" component={Login} sensitive={true} />
-            <Route path="/Dashboard" component={Dashboard} />
-            <Route path="/Edit" component={Edit} />
-            />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/Home">
+            <Redirect to="/" />
+          </Route>
+          <Route path="/Login" component={Login} sensitive={true} />
+          <Route path="/Dashboard" component={Dashboard} />
+          <Route path="/Edit" component={Edit} />
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
