86e7ec4d004a7e02689c84e549fc053b64b44d88
Alex <alxford45@gmail.com>
2019-06-28 18:05:32 -0500

    added setTitle to reducer;  removed index property from Action type; updated reducer logic to handle payload union typing;

--- web/src/context/list.reducer.tsx
+++ web/src/context/list.reducer.tsx
@@ -1,38 +1,58 @@
-import { Artist } from "../list/Artist";
+import { Artist, isArtist } from "../list/Artist";
 import { type } from "os";
+import { ArtistList } from "../list/ArtistList";
 
 export const ADD_ARTIST = "ADD_ARTIST";
 export const REMOVE_ARTIST = "REMOVE_ARTIST";
+export const SET_TITLE = "SET_TITLE";
 
 interface State {
+  title: string;
   artistList: Artist[];
 }
 interface Action {
   type: string;
-  payload?: Artist;
-  index?: number;
+  payload: Artist | string | number;
 }
 
 type ListReducer = (state: State, action: Action) => State;
 
 export const listReducer: ListReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_ARTIST": {
-      if (action.payload === undefined) {
-        console.log("error: artist not added because payload was undefined");
-        return state;
+  try {
+    switch (action.type) {
+      case "SET_TITLE": {
+        if (typeof action.payload !== "string") {
+          throw "Error: expected payload to be type 'string' for 'SET_TITLE'";
+        }
+        const title = action.payload;
+        return { title: title, artistList: state.artistList };
       }
-      const artist: Artist = action.payload;
-      return { artistList: [...state.artistList, artist] };
-    }
+      case "ADD_ARTIST": {
+        if (!isArtist(action.payload)) {
+          throw "Error: expected payload to be type 'Artist' for 'ADD_ARTIST'";
+        }
+        const artist: Artist = action.payload;
+        return {
+          title: state.title,
+          artistList: [...state.artistList, artist]
+        };
+      }
 
-    case "REMOVE_ARTIST": {
-      const artistList = state.artistList.filter(
-        (_, index) => index !== action.index
-      );
-      return { artistList: artistList };
+      case "REMOVE_ARTIST": {
+        if (typeof action.payload !== "number") {
+          throw "Error: expected payload to be type 'number' for 'REMOVE_ARTIST'";
+        }
+        const artistList = state.artistList.filter(
+          (_, index) => index !== action.payload
+        );
+        return { title: state.title, artistList: artistList };
+      }
+      default:
+        return state;
     }
-    default:
-      return state;
+  } catch (error) {
+    console.log(error);
+    console.log("Warning: returning previous state before error");
+    return state;
   }
 };
