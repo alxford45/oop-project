ee6ac410a64ea4ab78af97c46bdb5928982dd7eb
Alex <alxford45@gmail.com>
2019-06-28 18:02:26 -0500

    added title , setTitle properties to ListContext

--- web/src/context/List.context.tsx
+++ web/src/context/List.context.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Artist } from "../list/Artist";
 
 export const ListContext = React.createContext({
+  title: "",
   artistList: [] as Artist[],
   addArtist: (artist: Artist) => {},
-  removeArtist: (index: number) => {}
+  removeArtist: (index: number) => {},
+  setTitle: (title: string) => {}
 });
