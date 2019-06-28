e8f31a008c9e947792a04a7870996778ea259f30
Alex <alxford45@gmail.com>
2019-06-28 18:03:41 -0500

    defined title, setTitle in ListProvider

--- web/src/context/list.provider.tsx
+++ web/src/context/list.provider.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
-import { ADD_ARTIST, REMOVE_ARTIST, listReducer } from "./list.reducer";
+import {
+  ADD_ARTIST,
+  REMOVE_ARTIST,
+  SET_TITLE,
+  listReducer
+} from "./list.reducer";
 import { ListContext } from "./List.context";
 import { Artist } from "../list/Artist";
 
 export const ListProvider = props => {
   const [state, dispatch] = React.useReducer(listReducer, {
+    title: "",
     artistList: [] as Artist[]
   });
   const addArtist = (artist: Artist) => {
     dispatch({ type: ADD_ARTIST, payload: artist });
   };
   const removeArtist = (index: number) => {
-    dispatch({ type: REMOVE_ARTIST, index: index });
+    dispatch({ type: REMOVE_ARTIST, payload: index });
+  };
+  const setTitle = (title: string) => {
+    dispatch({ type: SET_TITLE, payload: title });
   };
   return (
     <ListContext.Provider
       value={{
+        title: state.title,
         artistList: state.artistList,
         addArtist: addArtist,
-        removeArtist: removeArtist
+        removeArtist: removeArtist,
+        setTitle: setTitle
       }}
     >
       {props.children}
