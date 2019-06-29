56a1108747492efff2bf4d6dbe5787fc9e1e4e7f
Alex <alxford45@gmail.com>
2019-06-28 20:13:54 -0500

    encapsulated onQuery in SearchResults with useEffect to query each request in async & re-render on request change

---
+++ web/src/pages/Edit/SearchResults.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { onQuery } from "./functions/onQuery";
+import { ListContext } from "../../context/List.context";
+
+export const SearchResults = (name: string) => {
+  React.useEffect(() => {
+    console.log("SearchResults Mounted");
+    return () => {
+      console.log("SearchResults Unmounted");
+    };
+  }, []);
+  const context = React.useContext(ListContext);
+  const addArtist = context.addArtist;
+  const [data, setData] = React.useState();
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await onQuery(name, addArtist);
+      setData(result);
+    };
+    console.log("firing query");
+    fetchData();
+  }, [onQuery, name]);
+  return <React.Fragment>{data}</React.Fragment>;
+};
