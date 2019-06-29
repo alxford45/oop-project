6eafa0e90302394efa7fc77d62558f966f0a0864
Alex <alxford45@gmail.com>
2019-06-29 18:57:52 -0500

    merged onCreate/onSave logic; Isolated mutation logic to only contain a Button

--- web/src/pages/Edit/Edit.tsx
+++ web/src/pages/Edit/Edit.tsx
@@ -3,23 +3,30 @@ import { Search } from "./SearchInput";
 import { ListComponent } from "./ListComponent";
 import { Title } from "./Title";
 import { SearchResults } from "./SearchResults";
+import { onCreate } from "./functions/onCreate";
+import { Button } from "@material-ui/core";
+import { ListContext } from "../../context/List.context";
+import useReactRouter from "use-react-router";
+import { History } from "history";
 
-export const Edit = () => {
+export const Edit = props => {
   React.useEffect(() => {
     console.log("Edit Mounted");
     return () => {
       console.log("Edit Unmounted");
     };
   }, []);
-
   let [searchInput, renderSearch] = Search();
-
+  const context = React.useContext(ListContext);
+  const list = context.artistList;
+  const title = context.title;
   return (
     <React.Fragment>
       {Title()}
       {renderSearch}
       {SearchResults(searchInput as string)}
       {ListComponent()}
+      {onCreate(list, title, props)}
     </React.Fragment>
   );
 };
--- web/src/pages/Edit/functions/onCreate.tsx
+++ web/src/pages/Edit/functions/onCreate.tsx
@@ -1,52 +1,32 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  InputBase
-} from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import * as React from "react";
 import { Mutation } from "react-apollo";
 import { createList } from "../../../graphql/mutations/createList";
-import { ArtistList } from "../../../list/ArtistList";
+import { Artist } from "../../../list/Artist";
+import SaveIcon from "@material-ui/icons/Save";
 
-export const onCreate = (list: ArtistList) => {
-  const [isOpen, setIsOpen] = React.useState(true);
-  const [title, setTitle] = React.useState("");
+export const onCreate = (list: Artist[], title: string, props: any) => {
+  const itemIds = list.flatMap(artist => artist.id);
+
   return (
-    <Mutation
-      mutation={createList}
-      onCompleted={data => list.setId(data.createList.id)}
-    >
+    <Mutation mutation={createList}>
       {createList => {
         return (
-          <Dialog open={isOpen} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Enter A List Title</DialogTitle>
-            <DialogContent>
-              <InputBase
-                autoFocus
-                margin="dense"
-                id="title"
-                fullWidth
-                onChange={event => setTitle(event.target.value)}
-                value={title}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                type="button"
-                color="primary"
-                onClick={() => {
-                  setIsOpen(isOpen => !isOpen);
-                  createList({ variables: { title: title } });
-                  list.setTitle(title);
-                }}
-              >
-                Create List
-              </Button>
-            </DialogActions>
-          </Dialog>
+          <IconButton
+            aria-label="save"
+            onClick={async () => {
+              console.log(itemIds);
+              await createList({
+                variables: {
+                  title: title,
+                  itemIds: itemIds
+                }
+              });
+              props.history.push("/Dashboard");
+            }}
+          >
+            <SaveIcon />
+          </IconButton>
         );
       }}
     </Mutation>
