473465bd499368e9d7c23b9fd5133bc031ce76d6
Alex <alxford45@gmail.com>
2019-06-29 18:52:13 -0500

    changed client mutation to reflect ealier server mutation change

--- web/src/graphql/mutations/createList.ts
+++ web/src/graphql/mutations/createList.ts
@@ -1,6 +1,6 @@
 import { gql } from "apollo-boost";
 export const createList = gql`
-  mutation createList($title: String!) {
-    createList(title: $title) 
+  mutation createList($title: String!, $itemIds: [String!]!) {
+    createList(title: $title, itemIds: $itemIds)
   }
 `;
