05a4c4b482a01bd7b79bbaff06e5eca411e93205
Alex <alxford45@gmail.com>
2019-06-29 18:50:53 -0500

    changed BrowserRouter to Router to test using History object

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,24 @@
 import * as React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch
-} from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
+import { ListProvider } from "./context/list.provider";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { Edit } from "./pages/Edit/Edit";
 import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
-import { ListProvider } from "./context/list.provider";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 const App = () => {
+  console.log(history);
   return (
-    <Router>
+    <Router history={history}>
       <ListProvider>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/Home">
-            <Redirect to="/" />
-          </Route>
           <Route path="/Login" component={Login} sensitive={true} />
           <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/Edit" component={Edit} />
-          />
+          <Route path="/Edit" {...history} component={Edit} />
         </Switch>
       </ListProvider>
     </Router>
