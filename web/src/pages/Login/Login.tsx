981fa75d9d8426e263f1bfc5141e15377b9c78f6
Alex <alxford45@gmail.com>
2019-07-01 09:34:23 -0500

    Added update prop and added client.resetStore() to mutation function

--- web/src/pages/Login/Login.tsx
+++ web/src/pages/Login/Login.tsx
@@ -3,20 +3,34 @@ import { Mutation } from "react-apollo";
 import Form from "./Form";
 import { login } from "../../graphql/mutations/login";
 import { RouteComponentProps } from "react-router";
+import { me } from "../../graphql/queries/me";
 
 class Login extends React.Component<RouteComponentProps<{}>> {
   render() {
     return (
       <Mutation
         mutation={login}
-        onCompleted={() => this.props.history.push("/Dashboard")}
+        update={(cache, { data }) => {
+          if (!data || !data.login) {
+            console.log(data);
+            return;
+          }
+
+          cache.writeQuery({
+            query: me,
+            data: { me: data.login }
+          });
+        }}
       >
-        {mutate => (
+        {(login, { client }) => (
           <Form
             onSubmit={async data => {
-              const response = await mutate({
+              await client.resetStore();
+              const response = await login({
                 variables: data
               });
+              console.log("Response from Login mutation:");
+              console.log(response);
               this.props.history.push("/Dashboard");
             }}
           />
