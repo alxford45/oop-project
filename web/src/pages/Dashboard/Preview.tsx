0efe5ae7232d8dfa4fc8bac7f3951c9ce618c13a
Alex <alxford45@gmail.com>
2019-07-05 02:42:06 -0500

    Added PreviewState export to provide Events with list data

--- web/src/pages/Dashboard/Preview.tsx
+++ web/src/pages/Dashboard/Preview.tsx
@@ -8,26 +8,46 @@ import * as React from "react";
 import { Query } from "react-apollo";
 import { artistsQuery } from "../../graphql/queries/artistsQuery";
 
+interface List {
+  names: string[];
+}
+
+interface State {
+  lists: List[];
+}
+
+export const state: State = {
+  lists: [] as List[]
+};
 export const Preview = (ids: string[]) => {
-  let args = ids.filter((_, index) => index < 3);
+  //let args = ids.filter((_, index) => index < 3);
+
   return (
-    <Query query={artistsQuery} variables={{ ids: args }}>
+    <Query query={artistsQuery} variables={{ ids: ids }}>
       {({ data, loading, error }) => {
         if (error) {
           console.log(error);
         }
         if (loading) return null;
+
+        const list: List = { names: [] as string[] };
+        state.lists.push(list);
         return (
           <React.Fragment>
             {data.artists.artists.map((artist, index) => {
-              return (
-                <ListItem key={index}>
-                  <ListItemAvatar>
-                    <Avatar src={artist.images[0].url} />
-                  </ListItemAvatar>
-                  <ListItemText>{artist.name}</ListItemText>
-                </ListItem>
-              );
+              list.names.push(artist.name);
+              if (list.names.length < 4) {
+                return (
+                  <ListItem key={index}>
+                    <ListItemAvatar>
+                      <Avatar src={artist.images[0].url} />
+                    </ListItemAvatar>
+                    <ListItemText>{artist.name}</ListItemText>
+                  </ListItem>
+                );
+              } else {
+                return null;
+              }
             })}
           </React.Fragment>
         );
