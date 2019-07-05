79cbde35c3eb9c73d20879e2839baa9dc09bed01
Alex <alxford45@gmail.com>
2019-07-05 02:38:53 -0500

    added event context and routed Event (aka Collection)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import { ListProvider } from "./context/list.provider";
+import { ListProvider } from "./context/list/list.provider";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { Edit } from "./pages/Edit/Edit";
 import Home from "./pages/Home";
 import Login from "./pages/Login/Login";
 import { createBrowserHistory } from "history";
 import { View } from "./pages/View/View";
+import { Events } from "./pages/Events/Events";
+import { EventProvider } from "./context/event/event.provider";
 
 const history = createBrowserHistory();
 
@@ -14,13 +16,16 @@ const App = () => {
   return (
     <Router history={history}>
       <ListProvider>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/Login" component={Login} sensitive={true} />
-          <Route path="/Dashboard" component={Dashboard} />
-          <Route path="/Edit" {...history} component={Edit} />
-          <Route path="/View" {...history} component={View} />
-        </Switch>
+        <EventProvider>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Login" component={Login} sensitive={true} />
+            <Route path="/Dashboard" component={Dashboard} />
+            <Route path="/Edit" {...history} component={Edit} />
+            <Route path="/View" {...history} component={View} />
+            <Route path="/Events" {...history} component={Events} />
+          </Switch>
+        </EventProvider>
       </ListProvider>
     </Router>
   );
